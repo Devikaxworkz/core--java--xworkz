676f6d752a3861dc5393e86a6eaf1c33c80ef86d
Devikaxworkz <Devikashetty@29>
2023-07-12 14:18:56 +0530

    adding zomato files crud

---
+++ Zomato.java.java
@@ -0,0 +1,32 @@
+class Zomato{
+
+
+//purpose//functionality//behavior
+public static double search(String foodName){
+	
+if("pizza" == foodName){
+	System.out.println("Search food name is" + foodName);
+	return 99.00;
+}	
+	if("veg briyani" == foodName){
+		System.out.println("Search food name is " + foodName);
+		return 245.80;
+	}
+	if ("Burger" == foodName){
+		System.out.println("search food name is " + foodName);
+		
+		return 356.00;
+	}
+	if("rooti" == foodName0){
+		System.out.println("Search food name is " + foodName);
+		return 56.98;
+	}
+	if("chapati" == foodName){
+		System.out.println("Search food name is " + foodName);
+		return 43.56;
+	}
+		
+}
+                           //..............    methods
+// logic must be return inside the java
+}
